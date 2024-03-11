185e7cacc280b09b29ebf072db15e17feb01d033
Jeremy Baxter <37813767+jt-is-coding@users.noreply.github.com>
2024-03-11 20:21:26 +0000

    setting up umbrella results, adding keys

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require "sinatra"
 require "sinatra/reloader"
 
+@pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
+@gmaps_key = ENV.fetch("GMAPS_KEY")
+@openai_key = ENV.fetch("OPENAI_API_KEY")
+
 get("/") do
 
   erb(:main)
@@ -13,9 +17,22 @@ get("/umbrella") do
 
 end
 
+get("/process_umbrella") do
+
+  @location = params.fetch("user_location")
+  erb(:process_umbrella)
+
+end
+
 get("/message") do
 
   erb(:message)
+
+end
+
+get("/process_single_message") do
+
+  erb(:process_single_message)
 
 end
 
